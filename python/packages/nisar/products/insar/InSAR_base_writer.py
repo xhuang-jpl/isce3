ed6804a5909e422cfb5b7171005d1870b35a697f
Xiaodong Huang <xhuang@nisar-adt-dev-5.jpl.nasa.gov>
2025-08-21 01:13:49 +0000

    update the string

--- python/packages/nisar/products/insar/InSAR_base_writer.py
+++ python/packages/nisar/products/insar/InSAR_base_writer.py
@@ -1112,7 +1112,7 @@ class InSARBaseWriter(h5py.File):
 
             # Update the description for the listOfObservationModes
             ds_name = f"{rslc_name}ListOfObservationModes"
-            description = 'List of observation modes of the L0B datasets'+\
+            description = 'List of observation modes of the L0B granules'+\
                 f' used to generate the {rslc_name} RSLC (one mode per L0B)'
             if ds_name in dst_id_group:
                 ds = dst_id_group[ds_name]
